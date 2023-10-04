05154903674eeec5704d6dacc004b35ff3dfe7ae
bag0ly <balazsforgo98@gmail.com>
2023-10-04 19:30:53 +0200

    gyakorlas

---
+++ gyakorlas/gyakorlas/Dto.cs
@@ -0,0 +1,13 @@
+﻿namespace gyakorlas
+{
+    public class Dto
+    {
+        public record UserDto(Guid Id, string Name, int Age,
+            string Email, DateTimeOffset Created);
+        public record CreatedUserDto(string Name, int Age,
+            string Email);
+        public record UpdateUserDto(string Name, int Age,
+            string Email);
+        public record DeleteUserDto(Guid Id);
+    }
+}
---
+++ gyakorlas/gyakorlas/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace gyakorlas.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+        public DateTimeOffset Created { get; set; }
+    }
+}
